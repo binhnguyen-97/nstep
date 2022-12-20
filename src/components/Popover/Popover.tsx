a174bf8bc1cfcd3395b86276e035f38d60fe178e
Bình Nguyễn <binh.nguyen3@tiki.vn>
2022-12-20 18:50:05 +0700

    feat(popove): update Popover comp

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,24 +1,66 @@
-/**
- * Popover is a component display a *popper*, a small piece of info, when a user clicks or taps on what usually is a button.
- *
- * Let's make one that uses the Extendable as an atom ⚛️.
- *
- * Requirements:
- * - It must have all the functionalities of an Extendable
- * - It must be positioned **NEXT** to the triggering element, and the positioning could be customised.
- * - It must be mounted/unmounted from DOM completely instead of being visually hidden.
- * - Clicking/tapping on the trigger displays the popover.
- * - Clicking anywhere *outside* the popover closes it.
- * - It could be used standalone or controlled (*with/without external useState()*).
- * - The default trigger is a NstButton.
- *
- * **BONUS POINT 🎨**: Add a little transition where it fades in & out, and expand & shrink on mount/unmount (both from 90% -> 100%).
- *
- * **BONUS BONUS POINT 🦮**: Make it WCAG compliant.
- * > Good place to start: https://yale-a11y.gitlab.io/ui-component-library/tooltips-popovers
- * 
- * Hint if you are not a11y-familiar: focus management is important to accessible users.
- */
-export const NstPopover = () => {
-  return <></>;
-};
+import {
+  useState,
+  forwardRef,
+  useMemo,
+  Ref,
+  ReactNode,
+  Children,
+  cloneElement,
+  ReactElement,
+  isValidElement,
+} from 'react';
+import {
+  NstExtendable,
+  CustomizableExtendableProps,
+} from '@/components/Extendable';
+import { NstButton } from '../Button';
+
+interface NstPopoverProps extends CustomizableExtendableProps {
+  children: ReactNode;
+}
+
+export const NstPopover = forwardRef<Ref<unknown>, NstPopoverProps>(
+  ({ children, ...props }, ref) => {
+    const [triggerElement, setTriggerElement] = useState<HTMLElement>();
+
+    const triggerItem = useMemo(() => {
+      /**
+       * Default trigger item
+       */
+      if (!children)
+        return (
+          <NstButton ref={setTriggerElement as Ref<HTMLButtonElement>}>
+            Click me
+          </NstButton>
+        );
+
+      let triggerElement: ReactNode = null;
+      /**
+       * Only accept 1 child as trigger item
+       *  */
+      if (Children.count(children) > 1) {
+        triggerElement = Children.toArray(children)[0];
+      } else {
+        triggerElement = children as ReactElement;
+      }
+
+      if (!isValidElement(triggerElement))
+        return (
+          <NstButton ref={setTriggerElement as Ref<HTMLButtonElement>}>
+            Click me
+          </NstButton>
+        );
+
+      return cloneElement(triggerElement, {
+        ...triggerElement.props,
+        ref: setTriggerElement,
+      });
+    }, []);
+
+    return (
+      <NstExtendable ref={ref} {...props} referenceElement={triggerElement!}>
+        {triggerItem}
+      </NstExtendable>
+    );
+  }
+);
