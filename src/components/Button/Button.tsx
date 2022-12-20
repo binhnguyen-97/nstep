85b66cdcf29a89ca0fef67b837d386ee31ec5098
Bình Nguyễn <binh.nguyen3@tiki.vn>
2022-12-20 13:43:31 +0700

    feat(NstButton): custom button now accept all general button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,17 @@
-import type { ReactNode } from "react";
+import { twMerge } from 'tailwind-merge';
 
-
-export type NstButtonProps = {
-  /**
-   * Children is required on a button, at least either an icon or text.
-   */
-  children: ReactNode;
-};
+import type { HTMLAttributes, ReactNode } from 'react';
 
 /**
- * This is a simple example of a button 🖱. Let's try to make it satisfy all the requirements we stated in the design system.
- * 
- * You likely will run into a classic Tailwind problem here 😑.
- * >Feel free to use 3rd party library like [tailwind-merge](https://github.com/dcastil/tailwind-merge) if you want.
+ * Extend HTML button attribute
  */
-export const NstButton = (props: NstButtonProps) => {
-  return <button className="text-white px-4 py-2 bg-rose-500 shadow-sm border-rose-500 rounded-lg" {...props} />;
+export interface NstButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
 }
+
+export const NstButton = ({ className, ...props }: NstButtonProps) => {
+  const defaultClass =
+    'focusable text-white px-4 py-2 bg-rose-500 shadow-sm border-rose-500 rounded-lg hove:opacity-[0.8]';
+
+  return <button className={twMerge(defaultClass, className)} {...props} />;
+};
