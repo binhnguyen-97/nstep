42aeef53ef1272cca3a5672d39c7082a0d7e3d78
Bình Nguyễn <binh.nguyen3@tiki.vn>
2022-12-20 18:49:43 +0700

    feat(NstExtendable): Update Extenable component to match requirement

--- src/components/Extendable/Extendable.tsx
+++ src/components/Extendable/Extendable.tsx
@@ -1,24 +1,163 @@
-import { usePopper } from "react-popper";
+import {
+  useState,
+  forwardRef,
+  useMemo,
+  Children,
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  useEffect,
+} from 'react';
+import { usePopper } from 'react-popper';
+import { ResizeObserver } from 'resize-observer';
 
-/**
- * Popovers & Dropdowns are in a family of dynamic components whose position depends on the elements that *trigger* them,
- * since they are usually absolutely/fixed positioned *right next* to where the user just interacts 🎆.
- *
- * Positioning these dynamic elements is not trivial. Luckily, [popperJS](https://popper.js.org) takes care of that for you
- * quite easily 👍.
- *
- * Let's make a reusable component called Extendable that will takes care of popper-js integrations that will be the
- * atom for both Popover & Dropdown to build on top of 🎩.
- * 
- * Requirements:
- * - It could be attached to any element.
- * - It stays on top of all other elements on the page to ensure that its content is visible.
- * 
- * **BONUS POINT 💤**: Don't use z-index, z-index war is no fun for anyone.
- * 
- * **BONUS BONUS POINT 💗**: Popper-js already takes care of 90% of a11y & responsiveness for you, except for one case:
- * - If the size of the trigger changes, what happens?
- */
-export const NstExtendable = () => {
-  return <></>;
-};
+import useOnClickOutside from '@/hooks/useClickOutside';
+
+import type { Ref, ReactNode } from 'react';
+import type { Placement } from '@popperjs/core';
+
+export interface CustomizableExtendableProps {
+  placement?: Placement;
+  content: ReactNode;
+  trigger?: 'click' | 'hover';
+  forceHide?: boolean;
+  setForceHide?: Function;
+}
+
+interface NstExtendableProps extends CustomizableExtendableProps {
+  referenceElement: HTMLElement;
+  children?: React.ReactElement<ChildProps>;
+}
+
+interface ChildProps {
+  id: string;
+  ref?: number;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+  onMouseEnter?: (e: React.MouseEvent<HTMLElement>) => void;
+  onMouseLeave?: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const NstExtendable = forwardRef<Ref<unknown>, NstExtendableProps>(
+  (
+    {
+      referenceElement,
+      content,
+      placement = 'right',
+      children,
+      trigger = 'hover',
+      forceHide,
+      setForceHide,
+    },
+    ref
+  ) => {
+    const [popperElement, setPopperElement] = useState<HTMLElement>();
+    const [visible, setVisible] = useState<boolean>(false);
+
+    const { styles, attributes, forceUpdate } = usePopper(
+      referenceElement,
+      popperElement,
+      {
+        placement,
+        modifiers: [
+          {
+            name: 'eventListeners',
+            options: {
+              resize: true,
+            },
+          },
+        ],
+      }
+    );
+
+    useOnClickOutside(popperElement, () => setVisible(false));
+
+    /**
+     * Add resize Observer to watch any change of trigger element
+     * Cal forceUpdate to update popper position
+     */
+    useEffect(() => {
+      const ro = new ResizeObserver(async () => {
+        if (!forceUpdate) return;
+        await forceUpdate();
+      });
+
+      const triggerEle = document.getElementById('trigger-item')!;
+      ro.observe(triggerEle);
+
+      return () => ro.unobserve(triggerEle);
+    }, []);
+
+    useEffect(() => {
+      if (forceHide && visible) {
+        setVisible(false);
+      }
+
+      return () => setForceHide && setForceHide(false);
+    }, [forceHide]);
+
+    const getChildProps = (childProps: any) => {
+      const props: ChildProps = {
+        ...childProps,
+      };
+
+      const handleMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
+        if (childProps.onMouseEnter) {
+          childProps.onMouseEnter(e);
+        }
+        setVisible(true);
+      };
+
+      const handleMouseLeave = (e: React.MouseEvent<HTMLElement>) => {
+        if (childProps.onMouseLeave) {
+          childProps.onMouseLeave(e);
+        }
+        setVisible(false);
+      };
+
+      const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        if (childProps.onClick) {
+          childProps.onClick(e);
+        }
+        setVisible((prev) => !prev);
+      };
+
+      if (trigger === 'hover') {
+        props.onMouseEnter = handleMouseEnter;
+        props.onMouseLeave = handleMouseLeave;
+      } else if (trigger === 'click') {
+        props.onClick = handleClick;
+      }
+
+      return props;
+    };
+
+    const triggerItem = useMemo(() => {
+      if (!children) return null;
+
+      return Children.map(children, (child: ReactElement<ChildProps>) => {
+        if (!isValidElement(child)) return child;
+        return cloneElement(child, {
+          ...child.props,
+          ...getChildProps(child.props),
+          id: 'trigger-item',
+        });
+      });
+    }, [children]);
+
+    return (
+      <>
+        {triggerItem}
+        {visible && (
+          <div
+            ref={setPopperElement as Ref<HTMLDivElement>}
+            style={styles.popper}
+            className="isolate z-[1]"
+            {...attributes.popper}
+          >
+            {content}
+          </div>
+        )}
+      </>
+    );
+  }
+);
